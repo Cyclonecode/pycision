85b32f114e54c66a65771c73f936acc85506331e
Krister Andersson <cyclonecode@gmail.com>
2022-01-22 19:31:47 +0100

    wip

--- cision.py
+++ cision.py
@@ -1,47 +1,107 @@
+import requests
+from datetime import datetime
+
+
 class CisionService:
     id: int
-    page_size: 50
-    page_index: 1
-    language: None
-    types: []
+    page_size: int = 50
+    page_index: int = 1
+    items_per_page: int = 0
+    language: str = None
+    types: list = []
+    categories: list = []
+    keywords: list = []
+    regulatory: bool = None
+    must_have_media: bool = False
     CISION_FEED_URL = 'https://publish.ne.cision.com/papi/NewsFeed/{id}'
-    CISION_RELEASE_URL = 'http://publish.ne.cision.com/papi/Release/'
+    CISION_RELEASE_URL = 'http://publish.ne.cision.com/papi/Release/{id}'
     DEFAULT_TYPES = ['PRM']
 
-    def __init__(self, options):
-        print(options.get('id'))
-        print('constructor called', options.get('id'))
+    def __init__(self, options: dict):
+        categories = options.get('categories', [])
+        for i in range(len(categories)):
+            categories[i] = categories[i].lower()
+        keywords = options.get('keywords', [])
+        for i in range(len(keywords)):
+            keywords[i] = keywords[i].lower()
+        types = options.get('types', self.DEFAULT_TYPES)
+        for i in range(len(types)):
+            types[i] = types[i].upper()
         self.id = options.get('id')
         self.page_size = options.get('page_size', 50)
         self.page_index = options.get('page_index', 1)
+        self.items_per_page = options.get('items_per_page', 0)
         self.language = options.get('language', None)
-        self.types = options.get('information_type', self.DEFAULT_TYPES)
-        print(self.types)
+        self.types = types
+        self.categories = categories
+        self.keywords = keywords
+        self.regulatory = options.get('regulatory', None)
+        self.must_have_media = options.get('must_have_media', False)
 
-    def getFeed(self):
-        params={
-                    'PageSize': self.page_size,
-                    'PageIndex': self.page_index
-                }
-        print(params)
-        print(self.CISION_FEED_URL.format(id=self.id))
-        response = requests.get(self.CISION_FEED_URL.format(id=self.id), params={
+    def get_feed(self):
+        params = {
             'PageSize': self.page_size,
-            'PageIndex': self.page_index
+            'PageIndex': self.page_index,
+            'DetailLevel': 'detail',
+            'Regulatory': None if self.regulatory == None else f'{self.regulatory}'.lower()
+        }
+        response = requests.get(self.CISION_FEED_URL.format(id=self.id), params={
+            k: v for k, v in params.items() if v
         })
-        if response.status_code == 200:
-            return json.dumps(self.handleFeedResponse(response.json()))
+        if response.status_code in [200, 201]:
+            return self.handle_feed_response(response.json())
 
         return None
 
-    def handleFeedResponse(self, content):
+    def get_feed_item(self, id: str) -> dict:
+        """Returns feed item based on its encrypted id.
+
+        Arguments:
+        id -- EncryptedId of the release
+        """
+        response = requests.get(self.CISION_RELEASE_URL.format(id=id)).json()
+        return response.get('Release')
+
+    def handle_feed_response(self, content: dict) -> dict:
         items = content.get('Releases')
         for item in items[:]:
+            not_found = False
+            item['PublishDate'] = datetime.strptime(item.get('PublishDate')[0:item.get('PublishDate').index('T')], '%Y-%m-%d').date()
+            if self.must_have_media and not len(item.get('Images', [])):
+                print('removing based on media')
+                items.remove(item)
+                continue
             if item.get('InformationType') not in self.types:
-                print ('removing based on type')
+                print('removing based on type')
                 items.remove(item)
+                continue
             if self.language and item.get('LanguageCode') != self.language:
-                print ('removing basd on language')
+                print('removing basd on language')
                 items.remove(item)
-        print (len(items))
+                continue
+            if len(self.categories):
+                if not len(item.get('Categories', [])):
+                    items.remove(item)
+                    continue
+                for category in item.get('Categories', [{'Code': None}]):
+                    if category.get('Name').lower() not in self.categories:
+                        print('removing based on category')
+                        not_found = True
+                        break
+                if not_found:
+                    items.remove(item)
+                    continue
+
+            if len(self.keywords):
+                print(item.get('Keywords'))
+                if not len(item.get('Keywords', [])):
+                    items.remove(item)
+                    continue
+                for keyword in item.get('Keywords', []):
+                    print('keyword', keyword)
+                    if keyword.lower() not in self.keywords:
+                        print('removing based on keyword => ', item.get('Title'))
+                        items.remove(item)
+                        break
+
         return items
--- web.py
+++ web.py
@@ -1,30 +1,40 @@
-from flask import Flask
-import requests
-import json
+from flask import Flask, render_template
 from cision import CisionService
 
 app = Flask(__name__)
 
 service = CisionService(options={
     'id': 'A275C0BF733048FFAE9126ACA64DD08F',
-    'page_size': 2,
-    'items_per_page': 5,
+    'page_size': 100,
+    'items_per_page': 50,
     'language': 'sv',
-    'categories': ['foo', 'bar'],
-    'keywords': ['tag1', 'tag2'],
+    # 'categories': ['Regular pressreleases', 'BAR'],
+    # 'keywords': ['tag1', 'TAG2'],
+    'regulatory': None,
+    # 'must_have_media': True,
+    'sort_order': 'alpha',
+    'sort_direction': 'Ascending',
+    'date_format': '%Y-%m-%d',
 })
 
-@app.route('/')
-@app.route('/hello')
-def hello():
-    # Render the page
-    return "Hello Python!"
 
-@app.route('/page/<int:id>')
-def page(id):
-    return service.getFeed()
+@app.route('/')
+def index():
+    items = service.get_feed()
+    items = items if service.items_per_page == 0 else items[0:service.items_per_page]
+    print(len(items))
+    print(service.items_per_page)
+    return render_template('feed.html', items=items, options={
+        'mark_items': False,
+        'date_format': 'Y-m-d',
+    })
+
+
+@app.route('/press/<id>')
+def article(id: str) -> dict:
+    item = service.get_feed_item(f'{id}')
+    return render_template('article.html', item=item)
+
 
 if __name__ == '__main__':
-    # Run the app server on localhost:4449
-    app.run('localhost', 4449, debug=True)
-
+    app.run('localhost', 4000, debug=True)
